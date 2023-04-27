ca82ba7e5129273cb44fd56467a215272d154021
DerBot7214 <lengwenath1@gmail.com>
2023-04-27 17:59:41 +0200

    setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 if which pnpm >/dev/null ; then
     echo "pnpm is already installed."
 else
+    echo "Installing pnpm."
+
     if which curl >/dev/null ; then
         echo "Downloading via curl."
         curl -fsSL https://get.pnpm.io/install.sh | sh -
@@ -12,16 +14,15 @@ else
         exit 1
     fi
 
-    export PNPM_HOME="~/.local/share/pnpm"
-    case ":$PATH:" in
-        *":$PNPM_HOME:"*) ;;
-        *) export PATH="$PNPM_HOME:$PATH" ;;
-    esac
+    source ~/.bashrc
+    echo "pnpm is installed."
 fi
 
 if which python3 >/dev/null ; then
     echo "python3 is already installed."
 else
+    echo "Installing python3."
+
     if which curl >/dev/null ; then
         echo "Downloading via curl."
         curl -fsSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
@@ -32,6 +33,8 @@ else
         echo "Cannot download, neither wget nor curl is available."
         exit 1
     fi
+
+    echo "python3 is installed."
 fi
 
 
