9da6a4cc65feaa34e6d12eae968f4c7dc97c7dfd
DerBot7214 <lengwenath1@gmail.com>
2023-04-27 18:05:59 +0200

    setup.sh script

--- setup.sh
+++ setup.sh
@@ -39,6 +39,15 @@ fi
 
 if which pip3 >/dev/null ; then
     echo "pip3 is already installed."
+
+elif which ~/.local/bin/pip3 >/dev/null ; then
+    echo "pip3 is already installed."
+    echo "adding ~/.local/bin to PATH"
+
+    if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+        echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
+        source ~/.bashrc
+    fi
 else
     echo "Installing pip3."
 
