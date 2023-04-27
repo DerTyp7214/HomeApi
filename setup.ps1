5f04322f11bfcf0fc197fb3239fab2c8bb9e3972
DerBot7214 <lengwenath1@gmail.com>
2023-04-27 18:51:05 +0200

    check for python on windows

--- setup.ps1
+++ setup.ps1
@@ -12,11 +12,24 @@ if ($?) {
 $ErrorActionPreference = 'SilentlyContinue'
 node -v > $null 2>&1
 $ErrorActionPreference = 'Continue'
+
 if ($?) {
   Write-Host "nodejs already installed"
 } else {
   Write-Host "Installing nodejs"
   winget install -e --id OpenJS.Nodejs
+}
+
+# check if python3 is installed and install if not
+$ErrorActionPreference = 'SilentlyContinue'
+python3 -V > $null 2>&1
+$ErrorActionPreference = 'Continue'
+
+if ($?) {
+  Write-Host "python3 already installed"
+} else {
+  Write-Host "Installing python3"
+  winget install -e --id Python.Python.3.11
 }
 
 pnpm install
