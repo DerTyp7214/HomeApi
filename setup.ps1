52701b0f213938f9d58314d2b8e012b484cb8656
DerBot7214 <lengwenath1@gmail.com>
2023-04-27 18:26:35 +0200

    nodejs on windows

--- setup.ps1
+++ setup.ps1
@@ -9,6 +9,16 @@ if ($?) {
   Invoke-WebRequest https://get.pnpm.io/install.ps1 -useb | iex
 }
 
+$ErrorActionPreference = 'SilentlyContinue'
+node -v > $null 2>&1
+$ErrorActionPreference = 'Continue'
+if ($?) {
+  Write-Host "nodejs already installed"
+} else {
+  Write-Host "Installing nodejs"
+  winget install -e --id OpenJS.Nodejs
+}
+
 pnpm install
 pnpm install:api
 pnpm install:web
